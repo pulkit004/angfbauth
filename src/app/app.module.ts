9c76b55f062ab65ad22ffa58c761ba54fa54472d
Pulkit Verma <pulkitverma004@gmail.com>
2018-01-24 12:29:24 +0530

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule, AngularFireAuthProvider } from 'angularfire2/auth';
+
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAw1sJKhBgoZIfXZvKuJykztyXrkNb5ATU',
+  authDomain: 'angfirebaseauth.firebaseapp.com',
+  databaseURL: 'https://angfirebaseauth.firebaseio.com',
+  projectId: 'angfirebaseauth',
+  storageBucket: '',
+  messagingSenderId: '566300869277'
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    routes
+
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
